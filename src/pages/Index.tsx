ca69ce0cb70e17611b183ba0e75aad76cb7a85eb
ZiadMohamed10 <hamedzezo23@gmail.com>
2025-11-12 05:34:46 +0200

    edit previeww

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,4 +1,3 @@
-import Head from 'next/head';
 import { ThemeToggle } from "@/components/ThemeToggle";
 import { Hero } from "@/components/Hero";
 import { Skills } from "@/components/Skills";
@@ -9,29 +8,15 @@ import { Footer } from "@/components/Footer";
 
 const Index = () => {
   return (
-    <>
-      <Head>
-        <title>Ziad Mohamed Portfolio</title>
-        <meta name="description" content="Front-end Developer Portfolio" />
-        <meta property="og:title" content="Ziad Mohamed Portfolio" />
-        <meta property="og:description" content="Front-end Developer Portfolio" />
-        <meta property="og:image" content="/public/preview.png" />
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://ziadmohamedportfolio.netlify.app/" />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:image" content="/images/new-preview.jpg" />
-      </Head>
-
-      <div className="min-h-screen transition-colors duration-500">
-        <ThemeToggle />
-        <Hero />
-        <About />
-        <Skills />
-        <Projects />
-        <Contact />
-        <Footer />
-      </div>
-    </>
+    <div className="min-h-screen transition-colors duration-500">
+      <ThemeToggle />
+      <Hero />
+      <About />
+      <Skills />
+      <Projects />
+      <Contact />
+      <Footer />
+    </div>
   );
 };
 
